5e8d1746d45ba481e9924e1de19f920686d85ecc
wooczak <kacper.luczak99@gmail.com>
2026-02-16 14:49:43 +0100

    Running code updates on mobile/desktop, animations and similar

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -7,53 +7,60 @@ export default function ContactForm() {
 
   return (
     <form onBlur={() => validateForm()} className={styles.contactForm}>
-      <label className={styles.label} htmlFor="name">
-        Your name
-      </label>
-      <input
-        className={`${styles.input} ${errors.name && !!formState.name ? styles.inputError : ""}`}
-        type="text"
-        id="name"
-        name="name"
-        value={formState.name}
-        onChange={({ target: { value } }) =>
-          dispatch({ type: "UPDATE_NAME_FIELD", value })
-        }
-      />
-      {errors.name && !!formState.name && (
-        <p className={styles.error}>{errors.name}</p>
-      )}
+      <div className={`${styles.wrapper} ${styles.nameWrapper}`}>
+        <label className={styles.label} htmlFor="name">
+          Your name
+        </label>
+        <input
+          className={`${styles.input} ${errors.name && !!formState.name ? styles.inputError : ""}`}
+          type="text"
+          id="name"
+          name="name"
+          value={formState.name}
+          onChange={({ target: { value } }) =>
+            dispatch({ type: "UPDATE_NAME_FIELD", value })
+          }
+        />
+        {errors.name && !!formState.name && (
+          <p className={styles.error}>{errors.name}</p>
+        )}
+      </div>
 
-      <label className={styles.label} htmlFor="email">
-        Your email
-      </label>
-      <input
-        className={`${styles.input} ${errors.email && !!formState.email ? styles.inputError : ""}`}
-        type="email"
-        id="email"
-        name="email"
-        onChange={({ target: { value } }) =>
-          dispatch({ type: "UPDATE_EMAIL_FIELD", value })
-        }
-        value={formState.email}
-      />
-      {errors.email && !!formState.email && (
-        <p className={styles.error}>{errors.email}</p>
-      )}
-      <label className={styles.label} htmlFor="message">
-        Your message
-      </label>
-      <textarea
-        className={styles.input}
-        id="message"
-        name="message"
-        value={formState.message}
-        onChange={({ target: { value } }) =>
-          dispatch({ type: "UPDATE_MESSAGE_FIELD", value })
-        }
-      ></textarea>
+      <div className={`${styles.wrapper} ${styles.emailWrapper}`}>
+        <label className={styles.label} htmlFor="email">
+          Your email
+        </label>
+        <input
+          className={`${styles.input} ${errors.email && !!formState.email ? styles.inputError : ""}`}
+          type="email"
+          id="email"
+          name="email"
+          onChange={({ target: { value } }) =>
+            dispatch({ type: "UPDATE_EMAIL_FIELD", value })
+          }
+          value={formState.email}
+        />
+        {errors.email && !!formState.email && (
+          <p className={styles.error}>{errors.email}</p>
+        )}
+      </div>
+
+      <div className={`${styles.wrapper} ${styles.messageWrapper}`}>
+        <label className={styles.label} htmlFor="message">
+          Your message
+        </label>
+        <textarea
+          className={`${styles.input} ${errors.message && !!formState.message ? styles.inputError : ""}`}
+          id="message"
+          name="message"
+          value={formState.message}
+          onChange={({ target: { value } }) =>
+            dispatch({ type: "UPDATE_MESSAGE_FIELD", value })
+          }
+        ></textarea>
+      </div>
       <button
-        className={`action-btn ${areErrors ? "action-btn--disabled" : ""}`}
+        className={`${styles.submitBtn} action-btn ${areErrors ? "action-btn--disabled" : ""} `}
         disabled={areErrors}
         type="submit"
       >
